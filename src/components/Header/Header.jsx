f1926993e8409f6353b722748fb7e65620cfccd5
Roman <albertushoffman@proton.me>
2023-08-24 11:38:22 +0300

    initialised project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+export const Header = () => {
+  return (
+		<header className='header'>
+			<a href="#">
+				<img src="../../assets/images/user-icon.png" alt="user icon" />
+			</a>
+		</header>
+	);
+};
