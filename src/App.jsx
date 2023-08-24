e8e7f695f7f37b4f114fea9bf5453505c00487ee
Roman <albertushoffman@proton.me>
2023-08-24 18:10:43 +0300

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './styles/reset.scss';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Sidebar />
+    </>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,39 @@
 import './Header.scss';
+import moment from 'moment';
+import userIcon from '../../assets/images/user-icon.png';
+import clockIcon from '../../assets/images/clock.png';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      let rawDate = new Date();
+
+      let currentDate = moment(rawDate).format('DD MMM YYYY').toUpperCase();
+      let currentTime = moment(rawDate).format('HH:mm');
+
+      setDate(currentDate);
+      setTime(currentTime);
+    }, 0);
+  }, []);
+
   return (
-		<header className='header'>
-			<a href="#">
-				<img src="../../assets/images/user-icon.png" alt="user icon" />
+    <header className='header'>
+      <a href='#' className='header__logo-and-title'>
+        <img src={userIcon} alt='user-logo' className='header__logo' />
+				<h1 className='header__title'>
+					Inventory
+				</h1>
 			</a>
-		</header>
-	);
+      <div className='header__date'>
+        <span className='header__today'>Today</span>
+        <span className='header__date'>{date}</span>
+        <img src={clockIcon} alt='clock icon' className='header__clock-icon' />
+				<span className='header__time'>{time}</span>
+      </div>
+    </header>
+  );
 };
