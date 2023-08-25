403d96ed6f929cc9dab6bd7e63cd5691a7ff6fbb
Roman <albertushoffman@proton.me>
2023-08-25 09:43:50 +0300

    edited sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,7 +7,14 @@ export const Sidebar = () => {
       <div className='sideabr__user'>
         <img src={userPhoto} alt='user photo' className='sidebar__user-photo' />
       </div>
-			
+			<article className="sidebar__links">
+				<a href="#" className='sidebar__link'>
+					Orders
+				</a>
+				<a href="#" className='sidebar__link'>
+					Products
+				</a>
+			</article>
     </section>
   );
 }
